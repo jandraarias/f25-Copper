05aa8f029db320f3fb7ac5571b32ffa02dd7bffc
William Poston <wpost003@odu.edu>
2025-09-16 18:11:49 -0400

    Remove Filament Auth for our own

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
+            ->authMiddleware(['auth', 'verified'])
             ->colors([
                 'primary' => Color::Amber,
             ])
