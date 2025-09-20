c3c506a4cb23440f92efcfa843fbbb27beaf42c4
Crystal Rivas <cvasq006@odu.edu>
2025-09-20 16:15:18 -0400

    Extend registration to create related Traveler record

--- routes/web.php
+++ routes/web.php
@@ -1,1 +1,24 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PreferenceController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/preferences/edit', [PreferenceController::class, 'edit'])->name('preferences.edit');
+    Route::post('/preferences/update', [PreferenceController::class, 'update'])->name('preferences.update');
+});
+
+require __DIR__.'/auth.php';
