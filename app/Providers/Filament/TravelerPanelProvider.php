b812f076ec3f3f2e42770bf73a0bcb94bcbc155c
William Poston <wpost003@odu.edu>
2025-09-22 15:53:03 -0400

    Travelers can create, edit, and delete personal items

--- app/Providers/Filament/TravelerPanelProvider.php
+++ app/Providers/Filament/TravelerPanelProvider.php
@@ -5,24 +5,19 @@ namespace App\Providers\Filament;
 use Filament\Panel;
 use Filament\PanelProvider;
 
-use App\Filament\Resources\Itineraries\ItineraryResource;
-use App\Filament\Resources\ItineraryItems\ItineraryItemResource;
-use App\Filament\Resources\PreferenceProfiles\PreferenceProfileResource;
-use App\Filament\Resources\Preferences\PreferenceResource;
-
 class TravelerPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
             ->id('traveler')
-            ->path('traveler')
+            ->path('traveler') // => base URL /traveler
             ->login()
             ->resources([
-                ItineraryResource::class,
-                ItineraryItemResource::class,
-                PreferenceProfileResource::class,
-                PreferenceResource::class,
+                \App\Filament\Resources\Itineraries\ItineraryResource::class,
+                \App\Filament\Resources\ItineraryItems\ItineraryItemResource::class,
+                \App\Filament\Resources\PreferenceProfiles\PreferenceProfileResource::class,
+                \App\Filament\Resources\Preferences\PreferenceResource::class,
             ])
             ->brandName('Itinerease')
             ->favicon(asset('favicon.ico'))
