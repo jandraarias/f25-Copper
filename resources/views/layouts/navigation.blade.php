7a527eff46c68ae55843d3da4025230eb7792d6e
William Poston <wpost003@odu.edu>
2025-09-24 16:02:05 -0400

    Remove Laravel Logo for ItinerEase

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ url('/') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <x-application-logo class="block text-2xl sm:text-[1.35rem] text-gray-900 dark:text-gray-100" />
                     </a>
                 </div>
 
