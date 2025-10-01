adecfb9a0fa17ab1ec6dd001625686a00c5c7d5c
William Poston <146898928+Willpatpost@users.noreply.github.com>
2025-10-01 11:07:29 -0400

    Fix import statement for PreferenceProfile model

--- app/Http/Controllers/PreferenceController.php
+++ app/Http/Controllers/PreferenceController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Preference;
-use App\Models\Preferenceprofile;
+use App\Models\PreferenceProfile;
 use Illuminate\Support\Facades\Auth;
 
 class PreferenceController extends Controller
