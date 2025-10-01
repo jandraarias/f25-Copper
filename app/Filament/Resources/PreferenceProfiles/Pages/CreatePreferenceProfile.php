65295eccc471e4a6be0d931a4bbda28c298c6cea
William Poston <146898928+Willpatpost@users.noreply.github.com>
2025-10-01 11:04:51 -0400

    Add files via upload

---
+++ app/Filament/Resources/PreferenceProfiles/Pages/CreatePreferenceProfile.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\PreferenceProfiles\Pages;
+
+use App\Filament\Resources\PreferenceProfiles\PreferenceProfileResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePreferenceProfile extends CreateRecord
+{
+    protected static string $resource = PreferenceProfileResource::class;
+}
